954ba19f87a7f18926034cda0208cb227084c064
IPlayDIY <124122826+IPlayDIY@users.noreply.github.com>
2023-02-17 16:42:30 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+  
+  myButton.onclick = function() {
+    setUserName();
+ }
+     
